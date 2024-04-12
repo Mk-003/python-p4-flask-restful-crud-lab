8464914488240fa05d4884c72bc29578babd003a
joseph <michealkinuthia.m.k@gmail.com>
2024-04-12 16:00:31 +0300

    patch, delete

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from urllib import response
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -44,8 +45,33 @@ api.add_resource(Plants, '/plants')
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        for attr in request.form:
+            setattr(plant, attr,request.form[attr])
+
+            db.session.add(plant)
+            db.session.commit()
+
+            response_dict= plant.to_dict()
+
+        
+        response=make_response(jsonify(plant), 200)
+
+        return response
+    
+    def delete(self,id):
+
+        plant=Plant.query.filter(Plant.id==id).first()
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        reponse_dict={'plant has been deleted'}
+        response_dict=make_response(
+            response_dict,
+            200
+        )
+        return response 
 
 
 api.add_resource(PlantByID, '/plants/<int:id>')
